1a98ab52a6c6dc7303bb79eb9995b12cbe95bb3f
shihxuan <C111196119@nkust.edu.tw>
2025-01-07 12:21:27 +0800

    C111196119

--- 旅遊+天氣.py
+++ 旅遊+天氣.py
@@ -6,8 +6,8 @@ from linebot.models import TextSendMessage, MessageEvent, TextMessage
 app = Flask(__name__)
 
 # 設定你的 Channel Access Token 和 Channel Secret
-LINE_CHANNEL_ACCESS_TOKEN = 'YD570GyXga2k3k1pb+fGOvh2SrWF0znyUaFuyFmMbNtcwh3EGMGIhOGFUkZ/PHHYo7eyqt3LQgNGm14YHJNkOncswAcnsiCU1/0XWwLcjjO9GjqbNEPgW7Et/geQUXOu+GgmYxkEP6JK/FRWPkuhpwdB04t89/1O/w1cDnyilFU='
-LINE_CHANNEL_SECRET = 'f7bef1fe3683d84b1050f118dd1ac016'
+LINE_CHANNEL_ACCESS_TOKEN = 'gM+wvmUdEhYU86zYXT5IPYxPj8r2rdzVkONURtTnofWRFi2hf/2hQOJiA59hFWhBr+Ds6J98URrFeRsc1tAL6jqJuPAPA+00Z5haeS1BHVOl5gxzeUI68i5rVgK0Nx8cvIwSQY3V7ZsVXDlMhxETcwdB04t89/1O/w1cDnyilFU='
+LINE_CHANNEL_SECRET = '7a5d3946285a4a0cce1312e6b1853bb0'
 
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
@@ -48,16 +48,35 @@ def handle_message(event):
     )
 
 def get_attractions(city):
+    # 假設這裡有一個函數來獲取城市的景點，可以用 API 或資料庫
+    # 這裡簡單回傳一些假資料
     attractions_dict = {
-        '台北': '台北101, 故宮博物院, 士林夜市',
-        '東京': '淺草寺, 東京塔, 秋葉原',
-        '紐約': '自由女神像, 中央公園, 大都會藝術博物館'
+        '臺北': '臺北101, 故宮博物院, 士林夜市',
+        '新北': '九份老街, 野柳地質公園, 平溪天燈節',
+        '基隆': '基隆廟口夜市, 正濱漁港, 和平島公園',
+        '桃園': '大溪老街, 小人國主題樂園, 拉拉山',
+        '新竹': '新竹城隍廟, 內灣老街, 六福村主題遊樂園',
+        '苗栗': '三義木雕博物館, 龍騰斷橋, 勝興車站',
+        '臺中': '逢甲夜市, 高美濕地, 彩虹眷村',
+        '彰化': '鹿港老街, 八卦山大佛, 芬園花海',
+        '南投': '日月潭, 清境農場, 溪頭自然教育園區',
+        '雲林': '北港朝天宮, 古坑綠色隧道, 成龍溼地',
+        '嘉義': '阿里山, 奮起湖老街, 檜意森活村',
+        '臺南': '赤崁樓, 安平古堡, 花園夜市',
+        '高雄': '蓮池潭, 駁二藝術特區, 六合夜市',
+        '屏東': '墾丁國家公園, 恆春古城, 四重溪溫泉',
+        '宜蘭': '羅東夜市, 蘇澳冷泉, 太平山森林遊樂區',
+        '花蓮': '太魯閣國家公園, 七星潭, 花蓮文化創意園區',
+        '臺東': '三仙台, 知本溫泉, 鹿野高台',
+        '澎湖': '七美雙心石滬, 馬公老街, 澎湖跨海大橋',
+        '金門': '翟山坑道, 莒光樓, 金門酒廠',
+        '馬祖': '北竿芹壁村, 東引燈塔, 南竿大漢據點'
     }
-    return attractions_dict.get(city, '抱歉，我不知道這個城市的景點。')
+    return attractions_dict.get(city, '無相關景點資料')
 
 def get_weather(city):
     mock_weather_data = {
-        "台北": {
+        "臺北": {
             "description": "多雲時晴",
             "temp_min": 20,
             "temp_max": 28,
@@ -69,7 +88,7 @@ def get_weather(city):
             "temp_max": 30,
             "rain_probability": 10
         },
-        "台中": {
+        "臺中": {
             "description": "短暫陣雨",
             "temp_min": 18,
             "temp_max": 25,
